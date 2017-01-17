4e1d7c9dadfda9dac7eb1d7900a336468f62ccab
carnelian <carnelian33@gmail.com>
2017-01-17 15:00:15 +0100

    update du css et affichage anniv via anniv.js

--- anniv.js
+++ anniv.js
@@ -3,13 +3,13 @@
 //requete en jquery pour recup fichier et mettre n json id de ma feuille : 1od20qYW8-q5nF1UArDO6kXUHP-O0BI4_t4Qr07x6bwI
 // lien de publication google sheets : https://docs.google.com/spreadsheets/d/1od20qYW8-q5nF1UArDO6kXUHP-O0BI4_t4Qr07x6bwI/pubhtml
 
-// ID of the Google Spreadsheet
+// ID de la Google Spreadsheet
  var spreadsheetID = "1od20qYW8-q5nF1UArDO6kXUHP-O0BI4_t4Qr07x6bwI";
  
- // Make sure it is public or set to Anyone with link can view 
+ // s'assurer que le tableau est public et publié sur le web
  var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
  
-// make JSON call to Google Data API
+// appel d'un json via Google Data API
 $.getJSON(url, function(data) {
 
 // debut création variable pour recuperer la date etc..
@@ -20,7 +20,7 @@ var anneeActuel = date.getFullYear();
 var moisActuel = date.getMonth() + 1;
 var jourActuel = date.getDate();
   
-  // loop to build html output for each row
+//
   var entry = data.feed.entry;
 
       for (var i = 0; i < entry.length; i++) {
@@ -28,14 +28,18 @@ var jourActuel = date.getDate();
         var jour = parseInt((entry[i]['gsx$jour']['$t']), 10);
         var mois = parseInt((entry[i]['gsx$mois']['$t']), 10);
         var prenom = entry[i]['gsx$prenom']['$t'];
+        var age = entry[i]['gsx$age']['$t'];
           
 // boucle a condition pour verif si anniv et affichage
         if ((jour === jourActuel) && (mois === moisActuel)){
  
-console.log("bon anniv " + prenom);
+            alert("bon anniv " + prenom);
+            
+            document.getElementById('fete').innerHTML = '<p>' + "Bon anniv " + prenom + ". Félicitation pour tes " + age + " ans" + '.</p>'
+            
         } else {
     //message pour les tests
-      console.log("sortie boucle");
+      
         
         }
       }
